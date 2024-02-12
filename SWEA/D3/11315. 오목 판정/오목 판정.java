89b348ac29db0f60866628572878418225f59d45
minukHwang <104661043+minukHwang@users.noreply.github.com>
2024-02-12 19:00:37 +0900

    [D3] Title: 오목 판정, Time: 167 ms, Memory: 24,788 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.java
+++ SWEA/D3/11315. 오목 판정/오목 판정.java
@@ -1,3 +1,11 @@
+/*
+ * [문제 풀이 과정]
+ * * 델타 탐색을 하면 되는 문제
+ * 1. 문자열을 받아서 2차원 배열에 할당한다.
+ * 2. 2차원 배열을 탐색하면서 만약 o를 만나게 된다면 해당 위치에서 8방 탐색을 진행한다.
+ * 3. 8방 중 한 방향으로 계속 탐색하면서 5개를 연달아서 o를 발견하게 된다면 오목 판정 YES
+ */
+
 import java.util.Scanner;
 
 public class Solution {
@@ -24,13 +32,16 @@ public class Solution {
 			out: for (int r = 0; r < n; r++) {
 				for (int c = 0; c < n; c++) {
 
+					// 만약 o를 발견한다면
 					if (map[r][c] == 'o') {
 						for (int i = 0; i < 8; i++) {
+							// 오목 판정을 위한 카운트
 							int count = 1;
 
 							int indexR = r + deltaR[i];
 							int indexC = c + deltaC[i];
 
+							// 범위 안에 있을 때까지 한 방향으로 카운트가 5가 되는지 확인
 							while (indexR >= 0 && indexR < n && indexC >= 0 && indexC < n) {
 
 								if (map[indexR][indexC] == 'o') {
