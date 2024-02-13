b5b5b4487268fb489e639b4b60a062448769e1f6
minukHwang <104661043+minukHwang@users.noreply.github.com>
2024-02-13 22:25:56 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 468 ms, Memory: 117228 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int count = 0;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out), 1 << 25);
+
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+
+		hanoiTower(N, 1, 3, 2);
+		System.out.println(count);
+		bw.flush();
+	}
+
+	public static void hanoiTower(int n, int from, int to, int other) throws IOException {
+		count++;
+		if (n == 1) {
+			bw.write(from + " " + to + "\n");
+			return;
+		}
+
+		hanoiTower(n - 1, from, other, to);
+		bw.write(from + " " + to + "\n");
+		hanoiTower(n - 1, other, to, from);
+	}
+}
