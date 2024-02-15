44f6cd62baf80a0835ccb6b86224982f69c67293
minukHwang <104661043+minukHwang@users.noreply.github.com>
2024-02-15 15:35:04 +0900

    [D3] Title: 농작물 수확하기, Time: 206 ms, Memory: 35,028 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,3 +1,12 @@
+/*
+ * [문제 풀이 과정]
+ * 1. 문자열을 2차원 배열에 입력 받는다.
+ * 2. left right 변수를 만들어서 2차원 배열의 c의 중심 index로 초기화를 해준다.
+ * 3. r이 중간까지 left는 -- right는 ++
+ * 4. 중간 이후로 left는 ++ right는 --
+ * 5. 해당 위치를 범위로 2차원 배열에 있는 숫자들을 더해주고 출력.
+ */
+
 import java.util.Scanner;
 
 public class Solution {
