152d137d1c75d7d7f2f1348749fcabdb902e2f7f
minukHwang <104661043+minukHwang@users.noreply.github.com>
2024-02-16 14:57:22 +0900

    [D4] Title: 러시아 국기 같은 깃발, Time: 150 ms, Memory: 22,784 KB -BaekjoonHub

--- SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
+++ SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
@@ -1,71 +1,121 @@
 import java.util.Scanner;
 
 public class Solution {
+
+	// 백트래킹을 위한 static 변수.
+	static int N, M;
+	static int[] selected;
+	static boolean[] visited;
+	
+	// 깃발 색깔을 위한 static 변수.
+	static int[][] colorCount;
+	static int row;
+	static int col;
+	static int minCount;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		int T = sc.nextInt();
 
 		for (int t = 1; t <= T; t++) {
-			int N = sc.nextInt();
-			int M = sc.nextInt();
+			row = sc.nextInt();
+			col = sc.nextInt();
 
-			char[][] map = new char[N][M];
-			int[][] counts = new int[3][N];
+			// 깃발 데이터 받기
+			char[][] flag = new char[row+1][col];
+			colorCount = new int[row+1][3];
 
-			for (int r = 0; r < N; r++) {
-				char[] str = sc.next().toCharArray();
-				for (int c = 0; c < M; c++) {
-					map[r][c] = str[c];
-				}
+			for (int i = 1; i < row+1; i++) {
+				flag[i] = sc.next().toCharArray();
 			}
 
-			for (int r = 0; r < N; r++) {
-				for (int c = 0; c < M; c++) {
-					switch (map[r][c]) {
+			// 각 줄의 색깔 갯수 카운트하기
+			for (int i = 1; i < row+1; i++) {
+				for (int j = 0; j < col; j++) {
+					switch (flag[i][j]) {
 					case 'W':
-						counts[0][r]++;
+						colorCount[i][0]++;
 						break;
+
 					case 'B':
-						counts[1][r]++;
+						colorCount[i][1]++;
 						break;
+
 					case 'R':
-						counts[2][r]++;
+						colorCount[i][2]++;
 						break;
 					}
 				}
 			}
+			
+			
+			// 최소를 구하는 문제이므로 처음에 큰 값을 넣어주어야한다.
+			// 문제의 최악의 케이스는 모든 색을 바꾸는 것이니, row*col+1로 설정
+			minCount = row*col+1; 
+			
+			// 1~ row까지를 2가지 숫자를 선택한다. (분절점)
+			M = 2;
+			N = row;
+			
+			// 선택된 숫자들을 담을 배열
+			selected = new int[M];
+			// 이미 방문한 요소인지 아닌지를 체크하기 위한 배열
+			visited = new boolean[N + 1];
+			
 
-			int minCount = Integer.MAX_VALUE;
-			// for문 지옥으로 풀어보기...
-			// 추후에 백트래킹으로 다시 풀어볼게요...
+			// 백트래킹 재귀.
+			backtrack(2, 0);
+			System.out.printf("#%d %d\n",t,minCount);
+		}
+	}
 
-			for (int i = 1; i < N - 1; i++) {
-				for (int j = i + 1; j < N; j++) {
-
-					int sum = 0;
-					for (int w = 0; w < i; w++) {
-						sum += M - counts[0][w];
-					}
-
-					for (int b = i; b < j; b++) {
-						sum += M - counts[1][b];
-					}
-
-					for (int r = j; r < N; r++) {
-						sum += M - counts[2][r];
-					}
-
-					if (minCount > sum) {
-						minCount = sum;
-					}
-
-				}
+	static void backtrack(int start, int depth) {
+		if (depth == M) {
+			// 깃발 갯수 탐색을 위한 변수
+			// 이렇게 진행하면 1~ row까지를 2가지 숫자를 선택하여 1세트 완성
+			// [2,3] -> white는 1번줄만, blue는 2번줄, red는 3~끝까지
+			// [4,7] -> white는 1~3번줄, blue는 4~6, red는 7~끝까지
+			
+			int count = 0;
+			
+			// 흰색 칠해야하는 부분 카운트
+			for(int w = 1; w < selected[0]; w++) {
+				count += col - colorCount[w][0];
 			}
-
-			System.out.printf("#%d %d\n", t, minCount);
-
+			
+			// 파란색 칠해야하는 부분 카운트
+			for(int b = selected[0]; b < selected[1]; b++) {
+				
+				count += col - colorCount[b][1];
+			}
+			
+			// 빨간색 칠해야하는 부분 카운트
+			for(int r = selected[1]; r <= row; r++) {
+				count += col - colorCount[r][2];
+			}
+			
+			// 최소값 구하기
+			if(count < minCount) {
+				minCount = count;
+			}
+			
+			return;
 		}
 
+		// 시작부터 숫자 전체 크기까지 탐색
+		for (int i = start; i <= N; i++) {
+			// 만약 방문하지 않은 숫자(노드)라면,
+			if (!visited[i]) {
+				// 해당 숫자 방문 처리
+				visited[i] = true;
+				// 해당 숫자 선택 배열에 추가
+				selected[depth] = i;
+				// 다음으로 넘어가기 (트리로 구상하면, 자식으로 가면서, 방문한 숫자 외의 숫자로.)
+				backtrack(i + 1, depth + 1);
+				// 다시 부모로 올라오면, 방문 false
+				visited[i] = false;
+			}
+		}
 	}
 }
