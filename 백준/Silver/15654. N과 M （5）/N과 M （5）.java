d3ad0918703d03f9cc722d27084f046882d4314b
minukHwang <104661043+minukHwang@users.noreply.github.com>
2024-02-17 20:42:25 +0900

    [Silver III] Title: N과 M (5), Time: 956 ms, Memory: 44892 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int N;
+	static int M;
+	
+	static int[] numbers;
+	static boolean[] checked;
+	static int[] selNum;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		numbers = new int[N];
+		checked = new boolean[N];
+		selNum = new int[M];
+		
+		for(int i = 0; i < N; i++) {
+			numbers[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(numbers);
+		
+		perm(0);
+		
+		
+	}
+	
+	public static void perm(int depth) {
+		if(depth == M) {
+			StringBuilder sb = new StringBuilder();
+			for(int i = 0; i < selNum.length - 1; i++) {
+				sb.append(selNum[i]).append(" ");
+			}
+			sb.append(selNum[selNum.length-1]);
+			System.out.println(sb);
+			return;
+		}
+		
+		for(int i = 0; i < numbers.length; i++) {
+			if(!checked[i]) {
+				checked[i] = true;
+				selNum[depth] = numbers[i];
+				perm(depth+1);
+				checked[i] = false;
+			}
+		}
+		
+	}
+}
