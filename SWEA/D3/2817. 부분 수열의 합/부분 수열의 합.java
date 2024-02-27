8a7f28f39c927941cee15a9ad8f8976fb43a65cb
minukHwang <104661043+minukHwang@users.noreply.github.com>
2024-02-27 14:05:40 +0900

    [D3] Title: 부분 수열의 합, Time: 1,305 ms, Memory: 23,848 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -1,5 +1,21 @@
 import java.util.Scanner;
 
+/*
+ * [문제 풀이 과정]
+ * 1. 부분 집합을 구하기 위해서 재귀 함수를 구현한다.
+ * 	- selected boolean 배열을 만들어서 부분 집한 안에 들어가는 요소를 체크한다.
+ * 	- 재귀를 순회하면서 [true, false, false, false]와 같이 부분집합이 될 수 있는 요소 체크
+ * 	1) index -> 0에서 시작
+ * 	2) 해당 index true
+ * 	3) 다음 인덱스로 이동
+ * 	4) 해당 index true
+ *	...
+ *	기저 조건 -> 만약 index가 배열의 크기이면.
+ *	5) 해당 index false하고 다시 다음 인덱스로 이동 
+ * 2. 기저 조건에 도달하면 배열을 탐색하고 true의 index에서 숫자들은 합한 sum을 구한다.
+ * 3. 만약 sum이 k와 같다면 count++
+ */
+
 public class Solution {
 	static int N;
 	static int K;
