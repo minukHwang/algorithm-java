9130219618d23306274b28c2d0abfccdd6b550e0
minukHwang <104661043+minukHwang@users.noreply.github.com>
2024-02-28 11:57:16 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 155 ms, Memory: 20,592 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,39 +1,37 @@
-import java.util.Scanner;
-
 /*
- * 문제 풀이 과정
- * 1. 재귀 호출 함수 만들기
- * 	- n을 연속적으로 곱해주는 재귀 함수
- * 	- n,m을 동시에 함수의 매개변수로하고, m을 1씩 줄이면서 함수에 n을 곱한 값을 리턴한다.
- * 	- 기저 조건으로 만약 m이 1이 되면 n을 리턴하도록 한다.
- * 2. 출력하기
+ * [문제 풀이 과정]
+ * - 분할 정복 활용하기
+ * 1. 재귀적으로 반으로 나누어서 숫자들을 지속적으로 곱해나간다.
+ * 2. 반으로 나눈 부분들을 tmp 변수에 저장하여 곱해준다.
  */
+
+import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		for (int i = 0; i < 10; i++) {
+		for(int t = 0; t < 10; t++) {
 			int caseNum = sc.nextInt();
-
+			
 			int N = sc.nextInt();
 			int M = sc.nextInt();
-
-			int answer = power(N, M);
-
+			
+			int answer = pow(N, M);
+			
 			System.out.printf("#%d %d\n", caseNum, answer);
 		}
 	}
-
-	public static int power(int n, int m) {
-		// 기저 조건
-		// m이 1이 되는 시점에서 n의 개수가 n-1번 곱해져 있기 때문에 n을 리턴
-		if (m == 1) {
-			return n;
+	
+	public static int pow(int number, int count) {
+		if(count == 1) {
+			return number;
 		}
-
-		// 재귀 호출
-		return n * power(n, --m);
+		
+		int tmp = pow(number, (count / 2));
+		if(count % 2 == 0) {
+			return tmp * tmp;
+		} else {
+			return tmp * tmp * number;
+		}
 	}
-
 }
