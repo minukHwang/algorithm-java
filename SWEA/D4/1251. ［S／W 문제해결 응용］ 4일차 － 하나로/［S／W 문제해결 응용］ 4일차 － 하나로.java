9411f50a2b9f6d0f5132d42b3ff8d0b5025562b2
minukHwang <104661043+minukHwang@users.noreply.github.com>
2024-03-27 17:23:44 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 1,078 ms, Memory: 113,932 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,23 +1,27 @@
+/*
+ * [문제 풀이 과정]
+ * *크루스칼 알고리즘 구현*
+ * 1. 정점의 정보들을 먼저 입력 받는다.
+ * 2. 순열을 활용하여 간선들을 구하고 이를 리스트에 넣는다.
+ * 
+ * -------크루스칼---------
+ * 3. 리스트를 간선의 정보 중 거리를 기준으로 정렬한다.
+ * 4. 정렬된 간선을 탐색하면서, 만약 간선의 정점이 동일한 집합에 존재하는지 union-find 알고리즘으로 확인
+ * 5. 동일한 집합에 존재한다면 -> 사이클을 형성, 존재하지 않다면 사이클을 형성하지 않으므로
+ * 	  distance에 따른 전체 환경 부담금을 더해나간다.
+ * 6. 최종적으로 출력하면 끝.
+ */
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
+// 정점 클래스
 class Vertex {
 	int idx;
 	int x;
 	int y;
-
-	Vertex() {
-
-	}
-
-	Vertex(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
 
 	@Override
 	public String toString() {
@@ -25,6 +29,7 @@ class Vertex {
 	}
 }
 
+// 간선 클래스
 class Edge implements Comparable<Edge> {
 	Vertex A;
 	Vertex B;
@@ -51,23 +56,27 @@ class Edge implements Comparable<Edge> {
 		return "Edge [A= " + A + ", B= " + B + ", distance=" + distance + "]";
 	}
 
+	// 간선을 정렬하는 기준은 distance
 	@Override
 	public int compareTo(Edge o) {
+		// distance가 double이라 Double.compare 사용
 		return Double.compare(this.distance, o.distance);
 	}
 }
 
 public class Solution {
 	static int T, N;
-	static Vertex[] vertices;
+	static double cost;
 
+	// 순열을 위한 정보
 	static boolean[] visited;
 	static Vertex[] selected;
 
+	// 정점 배열, 간선 리스트
+	static Vertex[] vertices;
 	static List<Edge> edges;
 
-	static double cost;
-
+	// union-find를 위한 parent 배열ㄴ
 	static int p[];
 
 	public static void main(String[] args) {
@@ -82,52 +91,57 @@ public class Solution {
 
 			for (int i = 0; i < N; i++) {
 				vertices[i] = new Vertex();
+				
+				// 정점 넘버링 (추후 union-find를 위해서)ㄴ
 				vertices[i].idx = i;
 
+				// x좌표 설정
 				int x = sc.nextInt();
 				vertices[i].x = x;
 			}
 
 			for (int i = 0; i < N; i++) {
+				// y좌표 설정ㄴ
 				int y = sc.nextInt();
 				vertices[i].y = y;
 			}
 
+			// 2차원 배열써도 무방하지만, 순열로 2개 정점 구해서 간선 리스트에 추가하기
 			visited = new boolean[N];
 			selected = new Vertex[2];
 			edges = new ArrayList<>();
 
-			cost = sc.nextDouble();
-
 			perm(0, 0);
 
-//			System.out.println(edges);
+			cost = sc.nextDouble();
 
-			// 1. 정렬 방법 2개
-			// 1) Comparator
-//			Collections.sort(edges, new Comparator<Edge>() {
-//
-//				@Override
-//				public int compare(Edge o1, Edge o2) {
-//					// TODO Auto-generated method stub
-//					return Double.compare(o1.distance, o2.distance);
-//				}
-//			});
+			// 크루스칼 1. 리스트 정렬 (방법 2개)
+				// 1) Comparator
+//				Collections.sort(edges, new Comparator<Edge>() {
+//	
+//					@Override
+//					public int compare(Edge o1, Edge o2) {
+//						return Double.compare(o1.distance, o2.distance);
+//					}
+//				});
 
-			// 2) class내에 Comparable 사용 상단 참고
+				// 2) class내에 Comparable 사용 상단 참고
 			Collections.sort(edges);
 
+			// 크루스칼 2. 사이클이 발생안하게 간선 뽑기
+
+			// 대표 배열 만들기
 			p = new int[N];
 
-			// 2. makeSet
 			for (int i = 0; i < N; i++) {
 				p[i] = i;
 			}
 
+			
 			double answer = 0;
 			int pick = 0;
 
-			// 3. 정렬된 간선 탐색
+			// 정렬된 간선 탐색
 			for (int i = 0; i < edges.size(); i++) {
 				Edge edge = edges.get(i);
 				
@@ -137,10 +151,17 @@ public class Solution {
 				int pA = findSet(A.idx);
 				int pB = findSet(B.idx);
 
+				// 만약 다른 집합이라면
 				if (pA != pB) {
+					// 합치고, 환경 부담금 계산
 					union(pA, pB);
 					answer += cost * Math.pow(edge.distance, 2);
 					pick++;
+				}
+				
+				// 간선을 개수만큼 다 뽑았다면 끝내기 (시간 절약)
+				if(pick == (N - 1)) {
+					break;
 				}
 			}
 
