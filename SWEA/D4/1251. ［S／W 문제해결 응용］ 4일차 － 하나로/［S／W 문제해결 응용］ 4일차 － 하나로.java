d275ad998a3bb29e2af603c4667f7172a761cd7f
minukHwang <104661043+minukHwang@users.noreply.github.com>
2024-03-28 15:48:53 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 881 ms, Memory: 154,780 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,84 +1,61 @@
 /*
  * [문제 풀이 과정]
- * *크루스칼 알고리즘 구현*
+ * *프림 알고리즘 구현*
  * 1. 정점의 정보들을 먼저 입력 받는다.
- * 2. 조합을 활용하여 간선들을 구하고 이를 리스트에 넣는다.
+ * 2. 조합을 활용하여 간선들을 구하고 이를 리스트 배열 정점 index에 맞게 넣는다.
  * 
- * -------크루스칼---------
- * 3. 리스트를 간선의 정보 중 거리를 기준으로 정렬한다.
- * 4. 정렬된 간선을 탐색하면서, 만약 간선의 정점이 동일한 집합에 존재하는지 union-find 알고리즘으로 확인
- * 5. 동일한 집합에 존재한다면 -> 사이클을 형성, 존재하지 않다면 사이클을 형성하지 않으므로
- * 	  distance에 따른 전체 환경 부담금을 더해나간다.
- * 6. 최종적으로 출력하면 끝.
+ * -------프림---------
+ * 3. 방문 배열과 PriorityQueue를 만든다.
+ * 4. 0에서 시작하는 모든 간선을 pq에 넣는다.
+ * 5. pq가 다 비워질 때까지 반복하면서 pq에서 간선을 poll하고, 만약 간선의 종점이 방문처리 되었다면 continue
+ * 	  아니라면, 해당 간선의 거리의 값을 통해서 환경 부담금을 계산한다. (또한, 해당 간선의 종점 방문 처리)
+ * 6. 간선의 종점에서 출발하는 다른 간선들 중 종점이 방문 처리 되지 않는 간선들을 다시 pq에 넣는다. (이 과정까지를 반복)
+ * 7. 최종적으로 구해진 답을 출력한다.
  */
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
-// 정점 클래스
 class Vertex {
 	int idx;
 	int x;
 	int y;
-
-	@Override
-	public String toString() {
-		return "#" + idx + " (" + x + ", " + y + ")";
-	}
 }
 
-// 간선 클래스
 class Edge implements Comparable<Edge> {
-	Vertex A;
-	Vertex B;
+	Vertex start;
+	Vertex end;
 	double distance;
 
 	Edge() {
 
 	}
 
-	Edge(Vertex A, Vertex B) {
-		this.A = A;
-		this.B = B;
-		this.distance = getDistance(A, B);
+	public Edge(Vertex start, Vertex end) {
+		this.start = start;
+		this.end = end;
+		this.distance = getDistance(start, end);
 	}
 
-	private double getDistance(Vertex A, Vertex B) {
-		int dx = A.x - B.x;
-		int dy = A.y - B.y;
-		return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+	private double getDistance(Vertex start, Vertex end) {
+		return Math.sqrt(Math.pow(start.x - end.x, 2) + Math.pow(start.y - end.y, 2));
 	}
 
-	@Override
-	public String toString() {
-		return "Edge [A= " + A + ", B= " + B + ", distance=" + distance + "]";
-	}
-
-	// 간선을 정렬하는 기준은 distance
 	@Override
 	public int compareTo(Edge o) {
-		// distance가 double이라 Double.compare 사용
 		return Double.compare(this.distance, o.distance);
 	}
 }
 
 public class Solution {
 	static int T, N;
-	static double cost;
-
-	// 순열을 위한 정보
-	static boolean[] visited;
-	static Vertex[] selected;
-
-	// 정점 배열, 간선 리스트
 	static Vertex[] vertices;
-	static List<Edge> edges;
-
-	// union-find를 위한 parent 배열ㄴ
-	static int p[];
+	static List<Edge>[] edges;
+	
+	static boolean checked[];
+	static Vertex[] selected;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -87,118 +64,77 @@ public class Solution {
 
 		for (int t = 1; t <= T; t++) {
 			N = sc.nextInt();
-
 			vertices = new Vertex[N];
-
+			
 			for (int i = 0; i < N; i++) {
 				vertices[i] = new Vertex();
-				
-				// 정점 넘버링 (추후 union-find를 위해서)ㄴ
+
 				vertices[i].idx = i;
-
-				// x좌표 설정
-				int x = sc.nextInt();
-				vertices[i].x = x;
+				vertices[i].x = sc.nextInt();
 			}
 
 			for (int i = 0; i < N; i++) {
-				// y좌표 설정ㄴ
-				int y = sc.nextInt();
-				vertices[i].y = y;
+				vertices[i].y = sc.nextInt();
 			}
 
-			// 2차원 배열써도 무방하지만, 조합으로 2개 정점 구해서 간선 리스트에 추가하기
-			// 같은 두 정점을 연결하는 간선을 중복해서 고려할 필요가 없음.
-			visited = new boolean[N];
+			edges = new ArrayList[N];
+			
+			for(int i = 0; i < N; i++) {
+				edges[i] = new ArrayList<>();
+			}
+			
+			double cost = sc.nextDouble();
+			
+			checked = new boolean[N];
 			selected = new Vertex[2];
-			edges = new ArrayList<>();
-
+			
 			com(0, 0);
-
-			cost = sc.nextDouble();
-
-			// 크루스칼 1. 리스트 정렬 (방법 2개)
-				// 1) Comparator
-//				Collections.sort(edges, new Comparator<Edge>() {
-//	
-//					@Override
-//					public int compare(Edge o1, Edge o2) {
-//						return Double.compare(o1.distance, o2.distance);
-//					}
-//				});
-
-				// 2) class내에 Comparable 사용 상단 참고
-			Collections.sort(edges);
-
-			// 크루스칼 2. 사이클이 발생안하게 간선 뽑기
-
-			// 대표 배열 만들기
-			p = new int[N];
-
-			for (int i = 0; i < N; i++) {
-				p[i] = i;
-			}
-
+			
+			boolean[] visited = new boolean[N];
+			PriorityQueue<Edge> pq = new PriorityQueue<>();
+			
+			visited[0] = true;
+			pq.addAll(edges[0]);
 			
 			double answer = 0;
-			int pick = 0;
-
-			// 정렬된 간선 탐색
-			for (int i = 0; i < edges.size(); i++) {
-				Edge edge = edges.get(i);
+			
+			while(!pq.isEmpty()) {
+				Edge current = pq.poll();
+				Vertex currentEnd = current.end;
 				
-				Vertex A = edge.A;
-				Vertex B = edge.B;
-
-				int pA = findSet(A.idx);
-				int pB = findSet(B.idx);
-
-				// 만약 다른 집합이라면
-				if (pA != pB) {
-					// 합치고, 환경 부담금 계산
-					union(pA, pB);
-					answer += cost * Math.pow(edge.distance, 2);
-					pick++;
-				}
+				if(visited[currentEnd.idx]) continue;
 				
-				// 간선을 개수만큼 다 뽑았다면 끝내기 (시간 절약)
-				if(pick == (N - 1)) {
-					break;
+				answer += cost * Math.pow(current.distance, 2);
+				visited[currentEnd.idx] = true;
+				
+				for(Edge edge: edges[currentEnd.idx]) {
+					if(!visited[edge.end.idx]) {
+						pq.add(edge);
+					}
 				}
 			}
-
+			
 			System.out.printf("#%d %.0f\n", t, answer);
 		}
 	}
-
-	private static void union(int x, int y) {
-		p[y] = x;
-
-	}
-
-	private static int findSet(int x) {
-		if (x != p[x]) {
-			p[x] = findSet(p[x]);
-		}
-
-		return p[x];
-	}
-
-	// 조합 구하는 메소드
-	public static void com(int start, int depth) {
+	
+	private static void com(int start, int depth) {
 		if (depth == 2) {
-			// 2개의 정점 구하면 간선 리스트에 추가
-			Edge edge = new Edge(selected[0], selected[1]);
-			edges.add(edge);
+			Vertex A = selected[0];
+			Vertex B = selected[1];
+			
+			edges[A.idx].add(new Edge(A, B));
+			edges[B.idx].add(new Edge(B, A));
+			
 			return;
 		}
 
 		for (int i = start; i < N; i++) {
-			if (!visited[i]) {
-				visited[i] = true;
+			if (!checked[i]) {
+				checked[i] = true;
 				selected[depth] = vertices[i];
 				com(i + 1, depth + 1);
-				visited[i] = false;
+				checked[i] = false;
 			}
 		}
 	}
